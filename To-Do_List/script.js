3b75a423b56ef4d93fd27695a64e37f6efaef38c
Priyanshu <bothulk1620@gmail.com>
2024-05-02 14:47:47 +0530

    projects

---
+++ To-Do_List/script.js
@@ -0,0 +1,24 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+
+    var taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        var li = document.createElement("li");
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = "";
+
+        li.addEventListener("click", function() {
+            li.classList.toggle("completed");
+        });
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "delete-button";
+        deleteButton.addEventListener("click", function() {
+            taskList.removeChild(li);
+        });
+        li.appendChild(deleteButton);
+    }
+}
